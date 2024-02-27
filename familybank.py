1ecff322f601004ff0cb7907d759e482f7e3a244
Christy-Vennance <mashellechristina@gmail.com>
2024-02-27 20:27:57 +0300

    first commit

---
+++ familybank.py
@@ -0,0 +1,11 @@
+# this is a comment
+
+# variable_name = value
+
+first_name = "Christina" # snake_case
+second_name = "Vennance" #Camel_case
+
+print(first_name)
+
+#pythom3 <familybank.py>
+print(second_name)
